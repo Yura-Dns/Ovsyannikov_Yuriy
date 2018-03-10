0479380b9f8dd34c8ab5c4d3dfb1c1daa962df78
Yura-Dns <yura013013@mail.ru>
2018-03-10 15:40:43 +1000

    1.3. JavDoc. Code convetion.[#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,9 @@
 package yura_dns;
-
+/**
+	* Для вывода строки в консоль.
+	* Main.
+	* @param args - args.
+*/
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Без разницы что и привет мир");
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Yuriy Ovsyannikov (yura013013@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
