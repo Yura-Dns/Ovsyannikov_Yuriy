3f7449363df5663a8d2167005cd85c1392a2f4e8
Yura-Dns <yura013013@mail.ru>
2018-03-19 18:42:02 +1000

    3.1. Элементарный калькулятор.[#45382]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -3,10 +3,35 @@ package ru.job4j.calculate;
 public class Calculate {
     private double result;
 
-    public void add(double first, double seconds){
+    public void add(double first, double seconds) {
+        this.result = first + seconds;
+    }
+
+    public double getResult() {
+        return this.result;
+    }
+
+    public void Minus(double first, double seconds) {
+        this.result = first - seconds;
+    }
+
+    public double getResult2() {
+        return this.result;
+    }
+
+    public void multiplication(double first, double seconds) {
+        this.result = first * seconds;
+    }
+
+    public double getResult3() {
+        return this.result;
+    }
+
+    public void division(double first, double seconds) {
         this.result = first / seconds;
     }
-    public double getResult(){
+
+    public double getResult4() {
         return this.result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -13,4 +13,28 @@ public class CalculateTest {
         double expected = 2D;
         assertThat(result, is(expected));
     }
+    @Test
+    public void whenAddOneMinusOneThenZiro() {
+        Calculate calc = new Calculate();
+        calc.Minus(1D, 1D);
+        double result = calc.getResult2();
+        double expected = 0D;
+        assertThat(result, is(expected));
+    }
+    @Test
+    public void whenMultiplication() {
+        Calculate calc = new Calculate();
+        calc.multiplication(2D, 4D);
+        double result = calc.getResult3();
+        double expected = 8D;
+        assertThat(result, is(expected));
+    }
+    @Test
+    public void whenDivision() {
+        Calculate calc = new Calculate();
+        calc.division(8D, 2D);
+        double result = calc.getResult4();
+        double expected = 4D;
+        assertThat(result, is(expected));
+    }
 }
