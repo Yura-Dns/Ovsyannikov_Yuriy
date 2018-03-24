a66c89c862ee5ed46484c11e10f145def416ae4b
Yura-Dns <yura013013@mail.ru>
2018-03-24 13:09:10 +1000

    4.2. Максимум из двух чисел[#46433]

--- chapter_001/src/test/java/ru/job4j/calculate/M_V_Test.java
+++ chapter_001/src/test/java/ru/job4j/calculate/M_V_Test.java
@@ -2,18 +2,16 @@ package ru.job4j.calculate;
 /**
  * M_V_Test (Maximum_value_Test - Тест на определение максимального значения)
  */
-
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class M_V_Test {
     @Test
-    public void whenAddOnePlusOneThenTwo() {
+    public void maxValue() {
         M_V calc = new M_V();
-        calc.add(1, 4);
-        int result = calc.max();
-        int expected = 4;
+        int result = calc.max(1, 3);
+        int expected = 3;
         assertThat(result, is(expected));
     }
 }
